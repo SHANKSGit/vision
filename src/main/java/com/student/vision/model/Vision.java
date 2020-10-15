aa35aea9348f0422187b1a1e6264961bf126ca31
shanks <meisong@imagedt.com>
2020-10-15 21:59:37 +0800

    1

--- src/main/java/com/student/vision/model/Vision.java
+++ src/main/java/com/student/vision/model/Vision.java
@@ -16,22 +16,22 @@ public class Vision {
      * 右眼裸眼
      */
     @NotNull(message = "右眼裸眼不能为空")
-    private Float rEye;
+    private String rEye;
     /**
      * 左眼裸眼
      */
     @NotNull(message = "左眼裸眼不能为空")
-    private Float lEye;
+    private String lEye;
     /**
      * 右眼屈光
      */
     @NotNull(message = "右眼屈光不能为空")
-    private Float rEyeRef;
+    private String rEyeRef;
     /**
      * 左眼屈光
      */
     @NotNull(message = "左眼屈光不能为空")
-    private Float lEyeRef;
+    private String lEyeRef;
 
 
 }

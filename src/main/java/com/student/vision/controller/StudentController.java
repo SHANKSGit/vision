78b447392403e6eded3c059507af0c810d39f34c
shanks <meisong@imagedt.com>
2020-10-16 21:50:17 +0800

    1

--- src/main/java/com/student/vision/controller/StudentController.java
+++ src/main/java/com/student/vision/controller/StudentController.java
@@ -4,7 +4,9 @@ import com.github.pagehelper.Page;
 import com.student.vision.model.BaseQuery;
 import com.student.vision.model.BaseResp;
 import com.student.vision.model.Student;
+import com.student.vision.model.Vision;
 import com.student.vision.service.StudentService;
+import com.student.vision.service.VisionService;
 import com.student.vision.util.DocUtil;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,8 +15,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @RestController
 @RequestMapping("/student")
@@ -22,6 +24,8 @@ public class StudentController {
 
     @Resource
     private StudentService studentService;
+    @Resource
+    private VisionService  visionService;
 
 
     @PostMapping("/search")
@@ -74,18 +78,53 @@ public class StudentController {
 
     @GetMapping("/export")
     @ResponseBody
-    public void export(HttpServletRequest request, HttpServletResponse response){
+    public void export(HttpServletRequest request,
+                           HttpServletResponse response,
+                           @RequestParam("id")Long id
+                      ){
+        Student students= studentService.searchById(id);
+        if(students==null){
+            return;
+        }
+        //2、日历类的时间操作
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.get(Calendar.MONTH)+1);
+        System.out.println(calendar.get(Calendar.DATE));
+
         Map<String,String> dataMap = new HashMap<String,String>();
-        dataMap.put("workname", "单位名称");
-        dataMap.put("name1", "wuhui");
-        dataMap.put("name2", "azy");
-        dataMap.put("name3", "zyq");
-        dataMap.put("year", "2019");
-        dataMap.put("month", "08");
-        dataMap.put("day", "19");
-        String newWordName = "信息.doc";
+        dataMap.put("name", students.getName());
+        dataMap.put("year", calendar.get(Calendar.YEAR)+"");
+        dataMap.put("mouth", (calendar.get(Calendar.MONTH)+1)+"");
+        dataMap.put("day", calendar.get(Calendar.DATE)+"");
+        String newWordName = students.getName()+"配镜信息.doc";
+        List<Vision> visionList=visionService.search(students.getId(),1,12);
+        for(int i=1;i<=12;i++){
+            String date="";
+            String eye="";
+            String eyeRef="";
+            String glass="";
+            if(i<=visionList.size()){
+                Vision vision=visionList.get(i-1);
+                date = new SimpleDateFormat("yyyy-MM-dd").format(vision.getCreateTime());
+                String lEye=vision.getlEye()==null?"":vision.getlEye();
+                String rEye=vision.getrEye()==null?"":vision.getrEye();
+                String lEyeRef=vision.getlEyeRef()==null?"":vision.getlEyeRef();
+                String rEyeRef=vision.getrEyeRef()==null?"":vision.getrEyeRef();
+                String rGlass=vision.getrGlass()==null?"":vision.getrGlass();
+                String lGlass=vision.getlGlass()==null?"":vision.getlGlass();
+                eye="左:"+lEye+" 右:"+rEye;
+                eyeRef="左:"+lEyeRef+" 右:"+rEyeRef;
+                glass="左:"+lGlass+" 右:"+rGlass;
+            }
+            dataMap.put("date"+i,date);
+            dataMap.put("eye"+i,eye);
+            dataMap.put("eyeRef"+i,eyeRef);
+            dataMap.put("glass"+i,glass);
+        }
         //调用打印word的函数
         DocUtil.download(request,response,newWordName, dataMap);
+        students.setStatus(1);
+        studentService.update(students);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
